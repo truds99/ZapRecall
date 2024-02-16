4a1eab068c577e2e4cd9fbf71da0381f8082b335
truds99 <dudscardoso9@gmail.com>
2024-02-16 17:12:56 -0300

    expansion

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,14 +6,15 @@ export default function Card ({item, numb}) {
     const [isOpen, setIsOpen] = React.useState(false);
 
     return (
-        <div className="card">
-        {
-            isOpen ?
-                <div className="questionText">{item.question}</div> :
+        isOpen ?
+            <div className="card open">
+                <div className="questionText">{item.question}</div>
+                <ion-icon onClick={() => setIsOpen(!isOpen)} class="play" name="play-outline"></ion-icon>
+            </div>:
+            <div className="card">
                 <p>Question {numb}</p>
-        }
-
-            <ion-icon onClick={() => setIsOpen(!isOpen)} class="play" name="play-outline"></ion-icon>
-        </div>
+                <ion-icon onClick={() => setIsOpen(!isOpen)} class="play" name="play-outline"></ion-icon>
+            </div>
+       
     );
 }   
