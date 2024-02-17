713189c77744b465e0a1c10bb942a720b1ae1ecc
truds99 <dudscardoso9@gmail.com>
2024-02-16 22:35:38 -0300

    icons and text colored

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,13 @@
 import './ButtonStyle.css'
 
-export default function Button({color, text, done, setDone}) {
+export default function Button({
+    color, text, done, setDone, isAnswered, setIsAnswered, isOpen, setIsOpen, setIsRight}) {
 
     function userAnswer(option) {
         setDone(done + 1);
+        setIsAnswered(!isAnswered);
+        setIsOpen(!isOpen);
+        setIsRight(option);
     }
 
     return (
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,10 +3,13 @@ import React from 'react';
 import Button from '../Button/Button'
 
 
-export default function Card ({item, numb, done, setDone}) {
+export default function Card ({item, numb, done, setDone,}) {
 
     const [isOpen, setIsOpen] = React.useState(false);
     const [isTapped, setIsTapped] = React.useState(false);
+    const [isAnswered, setIsAnswered] = React.useState(false);
+    const [isRight, setIsRight] = React.useState("");
+    
 
     return (
         isOpen ?
@@ -14,9 +17,9 @@ export default function Card ({item, numb, done, setDone}) {
                 <div className="card open tapped">
                     <div className="questionText">{item.answer}</div>
                     <div className='buttons'>
-                        <Button color={"red"} text={"I didn't remember"} done={done} setDone={setDone}/>
-                        <Button color={"yellow"} text={"Almost didn't remember"} done={done} setDone={setDone}/>
-                        <Button color={"green"} text={"Zap!"} done={done} setDone={setDone}/>
+                        <Button color={"red"} text={"I didn't remember"} done={done} setDone={setDone} isAnswered={isAnswered} setIsAnswered={setIsAnswered} isOpen={isOpen} setIsOpen={setIsOpen} isRight={isRight} setIsRight={setIsRight}/>
+                        <Button color={"yellow"} text={"Almost didn't remember"} done={done} setDone={setDone} isAnswered={isAnswered} setIsAnswered={setIsAnswered} isOpen={isOpen} setIsOpen={setIsOpen} isRight={isRight} setIsRight={setIsRight}/>
+                        <Button color={"green"} text={"Zap!"} done={done} setDone={setDone} isAnswered={isAnswered} setIsAnswered={setIsAnswered} isOpen={isOpen} setIsOpen={setIsOpen} isRight={isRight} setIsRight={setIsRight}/>
                     </div>
                 </div>:
                 <div className="card open">
@@ -24,10 +27,15 @@ export default function Card ({item, numb, done, setDone}) {
                     <ion-icon onClick={() => setIsTapped(!isTapped)} class="arrow" name="arrow-redo-outline"></ion-icon>
                 </div>
             ):
+            (isAnswered ?
+                <div className={`card answered text${isRight}`}>
+                    <p>Question {numb}</p>
+                    <ion-icon class={`text${isRight}`} name={isRight === "red" ? "close-circle" : isRight === "yellow" ? "help-circle" : "checkmark-circle"}></ion-icon>
+                </div>:
                 <div className="card">
                     <p>Question {numb}</p>
                     <ion-icon onClick={() => setIsOpen(!isOpen)} class="play" name="play-outline"></ion-icon>
                 </div>
-       
+            )
     );
 }   
