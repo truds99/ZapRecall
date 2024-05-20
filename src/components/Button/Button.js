a7d78054917959c244b8bd87b139145baf3aa687
truds99 <dudscardoso9@gmail.com>
2024-05-20 19:51:52 -0300

    add styled-components to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,4 @@
-import './ButtonStyle.css'
+import styled from "styled-components";
 
 export default function Button
     ({  color, 
@@ -20,9 +20,44 @@ export default function Button
     }
 
     return (
-        <div className={`button ${color}`} onClick={(() => userAnswer(color))}>
-            {text}
-        </div>
-    )
+      <Wrapper onClick={() => userAnswer(color)} $color={color}>
+        {text}
+      </Wrapper>
+    );
 }
+
+const Wrapper = styled.div`
+  width: 170px;
+  height: 65px;
+  font-size: 16px;
+  font-weight: 400;
+  font-family: Recursive;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  margin-bottom: 6px;
+  text-align: center;
+  overflow: hidden;
+  background-color: ${(props) =>
+    props.$color === "red"
+      ? "#FF3030"
+      : props.$color === "green"
+      ? "#2FBE34"
+      : "#FF922E"};
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media (max-width: 800px) {
+      width: 85.17px;
+      height: 37.17px;
+      font-size: 12px;
+      font-weight: 400;
+      max-width: 28%;
+  }
+`;
+
 
