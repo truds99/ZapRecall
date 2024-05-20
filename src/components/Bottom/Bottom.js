68c75247eec27aa4b60103d8f7486c7b3741ec5b
truds99 <dudscardoso9@gmail.com>
2024-05-20 19:42:44 -0300

    add styled-components to bottom

--- src/components/Bottom/Bottom.js
+++ src/components/Bottom/Bottom.js
@@ -1,7 +1,7 @@
-import './BottomStyle.css'
 import emojiRigth from '../../assets/party.png'
 import emojiWrong from '../../assets/sad.png'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export default function Bottom({done, countHits, icons, cards, validValue}){
     
@@ -10,28 +10,123 @@ export default function Bottom({done, countHits, icons, cards, validValue}){
     }
 
     return (
-        <div className={`bottom ${done === 8 ? "bigger" : ''}`}>
-            {done === 8 ? 
-                (countHits >= validValue ?
-                    <>  
-                        <div className='messageTitle'>
-                            <img src={emojiRigth}></img>
-                            Congratulations!
-                        </div>
-                        <p className='messageText'>You did it</p>
-                    </> :
-                    <>
-                        <div className='messageTitle'>
-                            <img src={emojiWrong}></img>
-                            Well, not this time.
-                        </div>
-                        <p className='messageText someLeft'>There's still some left, but don't give up.</p>
-                    </>
-                ) : ''
-            }
+        <Wrapper $done={done} >
+            {done === 8 ? (
+                countHits >= validValue ? (
+                <>
+                    <div className="messageTitle">
+                        <img src={emojiRigth}></img>
+                        Congratulations!
+                    </div>
+                    <p>You did it</p>
+                </>
+                ) : (
+                <>
+                    <div className="messageTitle">
+                        <img src={emojiWrong}></img>
+                        Well, not this time.
+                    </div>
+                    <p className="someLeft">
+                        There's still some left, but don't give up.
+                    </p>
+                </>
+                )
+            ) : (
+                ""
+            )}
             {done}/8 ANSWERED
-            {icons ? <div className='icons'>{icons}</div> : ''}
-            {done === 8 ? <Link to='/' className='button' onClick={restart}>Restart recall</Link> : ''}
-        </div>
-    )
+            {icons ? <div>{icons}</div> : ""}
+            {done === 8 ? (
+                <Link to="/" onClick={restart}>
+                    Restart recall
+                </Link>
+            ) : (
+                ""
+            )}
+        </Wrapper>
+    );
 }
+
+const Wrapper = styled.div`
+  background-color: white;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  color: #333333;
+  width: 100vw;
+  height: ${props => props.$done === 8 ? '244px' : '102px'};
+  display: flex;
+  flex-direction: column;
+  justify-content: ${props => props.$done === 8 ? 'space-around' : 'center'};
+  align-items: center;
+  font-size: 32px;
+  font-family: Recursive;
+
+  * {
+    overflow-x: hidden;
+    box-sizing: border-box;
+  }
+
+  &>div {
+    display: flex;
+  }
+
+  & img {
+    height: 35px;
+    width: 35px;
+    margin-right: 8px;
+  }
+
+  .messageTitle {
+    display: flex;
+    font-size: 18px;
+    font-weight: 700;
+    color: #333333;
+    align-items: center;
+  }
+
+  & a {
+    width: 275px;
+    height: 40px;
+    min-width: 167px;
+    border-radius: 5px;
+    font-family: Recursive;
+    background-color: #fb6b6b;
+    font-size: 14px;
+    font-weight: 400;
+    color: white;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & p {
+    font-size: 28px;
+    min-height: 31px;
+  }
+
+  @media (max-width: 800px) {
+
+    min-height: 70px;
+    font-size: 22px;
+    
+
+    & p {
+      font-size: 19.5px;
+      min-height: 28px;
+      text-align: center;
+    }
+
+    & a {
+      min-width: 250px;
+      height: 40px;
+    }
+  }
+
+  @media (max-width: 370px) {
+    & p.someLeft {
+      min-height: 48px;
+    }
+  }
+`;
